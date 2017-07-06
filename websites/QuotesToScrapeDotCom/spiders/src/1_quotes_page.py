2a455216f961ac93520b051f4e68381e9c75fe2e
kchawla-pi <kc.insight.pi@gmail.com>
2017-07-06 15:50:16 -0400

    Added spider to extract all the quotes from single webpage

---
+++ websites/QuotesToScrapeDotCom/spiders/src/1_quotes_page.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = ['http://quotes.toscrape.com']
+    
+    def parse(self, response):
+        self.log(' '.join(["URL visited:", response.url]))
+        for quote in response.css('div.quote'):
+            item = {'author': quote.css('small.author::text').extract_first(),
+                    'text': quote.css('span.text::text').extract_first(),
+                    'tags': quote.css('a.tag::text').extract()
+                   }
+            yield item
